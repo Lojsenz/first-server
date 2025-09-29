a5f064e81c235f568eeafdd486aef13a02f09073
Bartosz Bonk <bartoszbonk44@gmail.com>
2025-09-29 07:14:07 +0000

    project V1

---
+++ index.js
@@ -0,0 +1,41 @@
+import { createServer } from "node:http";
+import { readFileSync } from "node:fs";
+import { URL } from "node:url";
+
+const index_html = readFileSync("index.html");
+const icon = readFileSync("favicon.ico")
+const port = 8000;
+const host = "localhost";
+
+
+const server = createServer((req, res) => {
+  const request_url = new URL(`http://${host}${req.url}`);
+  const path = request_url.pathname;
+
+  console.log(`Request: ${req.method} ${path}`);
+
+  if (path === "/") {
+    if (req.method !== "GET") {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method not allowed\n");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(index_html);
+    }}
+  else if (path === "/favicon.ico") {
+    if (req.method !== "GET") {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method not allowed\n");
+    } else {
+      res.writeHead(200, { "Content-Type": "image/x-icon" });
+      res.end(icon);
+    }
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Site not found!\n");
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
